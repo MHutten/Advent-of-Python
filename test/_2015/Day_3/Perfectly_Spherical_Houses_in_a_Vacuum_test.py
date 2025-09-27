435247a37bf558c94506eace565585eca72b65b2
MHutten <milan@DESKTOP-93V7PIS>
2025-09-27 17:17:03 +0200

    Renamed class

--- test/_2015/Day_3/Perfectly_Spherical_Houses_in_a_Vacuum_test.py
+++ test/_2015/Day_3/Perfectly_Spherical_Houses_in_a_Vacuum_test.py
@@ -6,7 +6,7 @@ sys.path.append(os.path.join(os.path.dirname(__file__), "../../.."))
 
 from source._2015.Day_3.Perfectly_Spherical_Houses_in_a_Vacuum import calculate_number_of_visited_houses, calculate_number_of_visited_houses_with_robo_santa
 
-class IWasToldThereWouldBeNoMathTestCase(TestCase):
+class PerfectlySphericalHousesinaVacuumTestCase(TestCase):
 
     def test__calculate_number_of_visited_houses__good_weather__east(self):
         self.assertEqual(calculate_number_of_visited_houses(">"), 2)
